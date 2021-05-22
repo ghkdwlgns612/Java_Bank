2c5715bb5c776d853d462b0f9f9c98e23cf1cecd
Hoony <68694844+ghkdwlgns612@users.noreply.github.com>
2021-05-22 21:00:04 +0900

    0522(random값 받기 및 break)

--- my_atm.java
+++ my_atm.java
@@ -5,25 +5,43 @@ import java.util.Scanner;
 
 public class my_atm {
 	public static void main(String[] args) {
-		String chk_bank = "1.국민은행\n2.신한은행\n3.우리은행\n";
-		String chk_work = "1.계좌개설\n2.계좌조회\n3.잔고조회\n4.입.출금\n";
+		String chk_bank = "1.국민은행\n2.신한은행\n3.우리은행\n4.나가기";
+		String chk_work = "1.계좌개설\n2.계좌조회\n3.잔고조회\n4.입.출금\n5.나가기";
 		
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		my_inform user = new my_inform();
+		my_inform user = null;
 		
-		System.out.println(chk_bank);
-		int bank_num = scanner.nextInt();
-		System.out.println(chk_work);
-		int work_num = scanner.nextInt();
+		int bank_num;
+		int work_num;
+		String account = "";
 		while(true)
 		{
-			switch (work_num) {
-				case 1: //계좌개설
-				case 2: //계좌조회
-				case 3: //잔고조회
-				case 4: //입.출금
-			}	
+			System.out.println(chk_bank);
+			bank_num = scanner.nextInt();
+			scanner.nextLine();
+			if (bank_num == 4)
+				break;
+			while(true)
+			{
+				System.out.println(chk_work);
+				work_num = scanner.nextInt();
+				scanner.nextLine();
+				if (work_num == 5)
+					{System.out.println("프로그램 종료"); break;}
+				switch (work_num) {
+					case 1: //계좌개설 random,이름,비밀번호 필요
+						while (true)
+						{
+							account = "" + (random.nextInt(90000) + 10000);
+							System.out.println(account);
+							break;
+						}
+					case 2: //계좌조회 계좌번호,비밀번호
+					case 3: //잔고조회 계좌번호,비밀번호
+					case 4: //입.출금 액수,계좌번호,비밀번호
+				}	
+			}
 		}
-	}
+}
 }
--- my_inform.java
+++ my_inform.java
@@ -2,4 +2,45 @@ package my_bank;
 
 public class my_inform {
 	static my_inform[][] arrBank = new my_inform[3][100];
+	static int[] bank_count = new int[arrBank.length];
+	
+	int money;
+	String name;
+	String account;
+	String phone;
+	String password;
+	
+	public my_inform() {};
+	
+	static my_inform checkDup(String account)
+	{
+		my_inform a;
+		return a;
+	}
+	static my_inform checkPhone(String phone)
+	{
+		my_inform a;
+		return a;
+	}
+	
+	static my_inform login(String account, String password)
+	{
+		
+	}
+	void deposit(int money)
+	{
+		
+	}
+	boolean withdraw(int money)
+	{
+		
+	}
+	void chekc_money()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		
+	}
 }
