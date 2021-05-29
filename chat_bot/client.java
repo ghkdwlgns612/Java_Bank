922e9b0c2fa68801e4c78f9114054dbfffcc1d24
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-05-29 11:39:41 +0900

    05/29

--- chat_bot/client.java
+++ chat_bot/client.java
@@ -1,22 +1,69 @@
 package chat_bot;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class client {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	   public static void main(String[] args) {
+		   
+	 
+	        Socket socket = new Socket();
+	        Scanner sc = new Scanner(System.in);
+	 
+	        InputStream is = null;
+	        InputStreamReader isr = null;
+	        BufferedReader br = null;
+	 
+	        OutputStream os = null;
+	        OutputStreamWriter osw = null;
+	        PrintWriter pw = null;
+	 
+	        try {
+	            socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), 6005));
+	            System.out.println("[client] connected with server");
+	 
+	            while (true) {
+	 
+	                is = socket.getInputStream();
+	                isr = new InputStreamReader(is, "UTF-8");
+	                br = new BufferedReader(isr);
+	 
+	                os = socket.getOutputStream();
+	                osw = new OutputStreamWriter(os, "UTF-8");
+	                pw = new PrintWriter(osw, true);
+	 
+	                // ÀÐ´Â°Å
+	                System.out.print(">>");
+	                String data = sc.nextLine();
+	 
+	                if ("exit".equals(data))
+	                    break;
+	 
+	                pw.println(data);
+	 
+	                data = br.readLine();
+	                System.out.println("<< " + data);
+	 
+	            }
+	 
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                if (socket != null && !socket.isClosed()) {
+	                    socket.close();
+	                }
+	            } catch (IOException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+	            }
+	 
+	            sc.close();
+	 
+	        }
+	 
+	    }
+	 
 }
--- chat_bot/server.java
+++ chat_bot/server.java
@@ -1,28 +1,67 @@
 package chat_bot;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class server {
-    public static final int PORT = 6077;
-	public static void main(String[] args) {
-		try{
+
+	public static int PORT = 6005;
+	public static void main(String[] args)
+	{
+        InputStream is = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+ 
+        OutputStream os = null;
+        OutputStreamWriter osw = null;
+        PrintWriter pw = null;
+        Scanner sc = new Scanner(System.in);
+        
+		try {
 			ServerSocket server_socket = new ServerSocket();
 			
-		}catch(IOException e)
-		{
+			InetAddress address = InetAddress.getLocalHost();
+			String host = address.getHostAddress();
+//			System.out.println(address); //LAPTOP-N1HNO2EV/192.168.191.134
+//			System.out.println(host); //192.168.191.134
+
+			server_socket.bind(new InetSocketAddress(host, PORT));
+			System.out.println("[server] binding " + host);
 			
+			Socket socket = server_socket.accept();
+			InetSocketAddress socketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+            System.out.println("[server] connected by client");
+            System.out.println("[server] Connect with " + socketAddress.getHostString() + " " + socket.getPort());
+			
+            while (true)
+            {
+                is = socket.getInputStream();
+                isr = new InputStreamReader(is, "UTF-8");
+                br = new BufferedReader(isr);
+ 
+                // outputStream 가져와서 (주 스트림) StreamWriter와 PrintWriter로 감싸준다 (보조 스트림)
+                os = socket.getOutputStream();
+                osw = new OutputStreamWriter(os, "UTF-8");
+                pw = new PrintWriter(osw, true);
+ 
+                String buffer = null;
+                buffer = br.readLine(); // Blocking
+                if (buffer == null) {
+ 
+                    // 정상종료 : remote socket close()
+                    // 메소드를 통해서 정상적으로 소켓을 닫은 경우
+                    System.out.println("[server] closed by client");
+                    break;
+ 
+                }
+ 
+                System.out.println("[server] recived : " + buffer);
+                pw.println(buffer);
+            }
+			
+		}catch(IOException e){
+			e.printStackTrace();
 		}
 	}
-
 }
